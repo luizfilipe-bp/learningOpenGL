7697755a07c62b8b261e2300dc065e1b316ac248
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2023-07-31 23:57:16 -0300

    finally a triangle

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -41,6 +41,9 @@ int main(void)
 
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
